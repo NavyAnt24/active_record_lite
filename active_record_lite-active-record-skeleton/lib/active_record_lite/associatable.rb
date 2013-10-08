8dcbb8e916469a156c7f3f2108da396926b3d6cc
App Academy Student <student@appacademy.io>
2013-10-07 17:48:35 -0700

    Working on belongs_to method

--- active_record_lite-active-record-skeleton/lib/active_record_lite/associatable.rb
+++ active_record_lite-active-record-skeleton/lib/active_record_lite/associatable.rb
@@ -4,25 +4,41 @@ require_relative './db_connection.rb'
 
 class AssocParams
   def other_class
+    @other_class.constantize
   end
 
   def other_table
+    other_class.table_name
   end
 end
 
 class BelongsToAssocParams < AssocParams
+  attr_reader :class_name, :primary_key, :foreign_key
+
   def initialize(name, params)
+    @class_name = params[:class_name] || name.camelize
+    @primary_key = params[:primary_key] || "id"
+    @foreign_key = params[:foreign_key] || "#{name.downcase}_id"
+    @other_class = params[:class_name] || name.camelize
   end
 
   def type
+    :belongs_to
   end
 end
 
 class HasManyAssocParams < AssocParams
+  attr_reader :class_name, :primary_key, :foreign_key
+
   def initialize(name, params, self_class)
+    @class_name = params[:class_name] || name.camelize
+    @primary_key = params[:primary_key] || "id"
+    @foreign_key = params[:foreign_key] || self_class.table_name + "_id"
+    @other_class = params[:other_class_name] || name.singularize.camelize
   end
 
   def type
+    :has_many
   end
 end
 
@@ -31,6 +47,15 @@ module Associatable
   end
 
   def belongs_to(name, params = {})
+    aps = BelongsToAssocParams.new(name, params, self.class)
+    object = DBConnection.execute(<<-SQL, aps.foreign_key)
+      SELECT
+      *
+      FROM
+      #{aps.other_table}
+      WHERE
+      aps.primary_key = ?
+    SQL
   end
 
   def has_many(name, params = {})
